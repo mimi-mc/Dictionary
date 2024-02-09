bbe5ff5f9bd5db84677415a34b42864d419eb2ea
Miruna Craciun <157888228+mimi-mc@users.noreply.github.com>
2024-02-09 18:38:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let wordList = []
 
+function addToWordList() {
+    wordList.push(document.getElementById("addInput").value);
+    console.log(wordList);
+    document.getElementById("addInput").value = "";
+}
+
+function SearchWord() {
+    let word = document.getElementById("searchInput").value;
+    let isFound = wordList.includes(word);
+    if (isFound) {
+        document.getElementById('outputMessage').innerText = `The word "${word}" was found.`;
+    } else {
+        document.getElementById('outputMessage').innerText = `The word "${word}" was not found!`;
+    }
+    document.getElementById("searchInput").value = "";
+}
